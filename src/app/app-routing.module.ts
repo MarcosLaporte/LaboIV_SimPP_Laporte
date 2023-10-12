1a52145dfb8e8703e895c508c4340be2335b3cfe
LapMarcos <marcoslaporte2015@gmail.com>
2023-10-12 12:13:09 -0300

    Conexion de componentes a App.Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { ActorAltaComponent } from './components/actor/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './components/actor/actor-listado/actor-listado.component';
+import { PeliculaAltaComponent } from './components/peliculas/pelicula-alta/pelicula-alta.component';
+import { PeliculaListadoComponent } from './components/peliculas/pelicula-listado/pelicula-listado.component';
 
 const routes: Routes = [
 	{ path: 'bienvenido', component: AppComponent },
 	{ path: 'busqueda', component: BusquedaComponent },
-	{ path: 'peliculas', loadChildren: () => import('./modules/peliculas/peliculas.module').then(m => m.PeliculasModule) },
-	{ path: 'actor', loadChildren: () => import('./modules/actor/actor.module').then(m => m.ActorModule) }, 
+	{ path: 'actor/alta', component: ActorAltaComponent },
+	{ path: 'actor/listado', component: ActorListadoComponent },
+	{ path: 'actor/actorPelicula', component: ActorListadoComponent },
+	{ path: 'pelicula/alta', component: PeliculaAltaComponent },
+	{ path: 'pelicula/listado', component: PeliculaListadoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,42 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { TablaPeliculaComponent } from './components/peliculas/tabla-pelicula/tabla-pelicula.component';
-import { DetallePeliculaComponent } from './components/peliculas/detalle-pelicula/detalle-pelicula.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { ActorAltaComponent } from './components/actor/actor-alta/actor-alta.component';
+import { TablaPaisesComponent } from 'src/app/components/tabla-paises/tabla-paises.component';
+import { ActorPeliculaComponent } from 'src/app/components/actor/actor-pelicula/actor-pelicula.component';
+import { ActorListadoComponent } from 'src/app/components/actor/actor-listado/actor-listado.component';
+import { TablaPeliculaComponent } from 'src/app/components/peliculas/tabla-pelicula/tabla-pelicula.component';
+import { DetallePeliculaComponent } from 'src/app/components/peliculas/detalle-pelicula/detalle-pelicula.component';
+import { PeliculaAltaComponent } from './components/peliculas/pelicula-alta/pelicula-alta.component';
+import { PeliculaListadoComponent } from './components/peliculas/pelicula-listado/pelicula-listado.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		BusquedaComponent,
+		TablaPaisesComponent,
+		ActorAltaComponent,
+  	ActorPeliculaComponent,
+		ActorListadoComponent,
+		PeliculaAltaComponent,
+		PeliculaListadoComponent,
 		TablaPeliculaComponent,
 		DetallePeliculaComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+		FormsModule,
 		provideFirebaseApp(() => initializeApp(environment.firebase)),
 		provideDatabase(() => getDatabase()),
 		provideFirestore(() => getFirestore()),
 		HttpClientModule,
-		provideStorage(() => getStorage())
+		provideStorage(() => getStorage()),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
