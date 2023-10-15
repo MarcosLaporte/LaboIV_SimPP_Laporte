c432ae76c9f72900bde78aa0d76bd0a3cf3e17c9
LapMarcos <marcoslaporte2015@gmail.com>
2023-10-15 01:02:39 -0300

    UPDATE busqueda

--- src/app/components/peliculas/tabla-pelicula/tabla-pelicula.component.ts
+++ src/app/components/peliculas/tabla-pelicula/tabla-pelicula.component.ts
@@ -4,6 +4,8 @@ import { DatabaseService } from 'src/app/services/database.service';
 import { DatePipe } from '@angular/common';
 import { DocumentReference, Timestamp } from '@angular/fire/firestore';
 import { Actor } from 'src/app/classes/actor';
+import { StorageService } from 'src/app/services/storage.service';
+import Swal from 'sweetalert2';
 
 @Component({
 	selector: 'app-tabla-pelicula',
@@ -15,21 +17,30 @@ import { Actor } from 'src/app/classes/actor';
 export class TablaPeliculaComponent {
 	@Output() peliculaEvent = new EventEmitter<Pelicula>();
 	peliculas: Array<Pelicula> = [];
-	srcFoto: string = "../../../../favicon.ico";
 
-	constructor(private dbService: DatabaseService) { }
+	constructor(private dbService: DatabaseService, private stService: StorageService) { }
 
 	async ngOnInit() {
+		Swal.fire({
+			imageUrl: '../../../../assets/loader.gif',
+			imageWidth: 300,
+			imageHeight: 300,
+			imageAlt: 'loader',
+			showConfirmButton: false,
+			allowOutsideClick: false
+		});
 		const arrAux = await this.dbService.traerDatos<Pelicula>('pelis');
 		arrAux.sort((p1, p2) => p1.titulo > p2.titulo ? 1 : -1);
 		arrAux.forEach(async peli => {
 			peli.estreno = peli.estreno instanceof Timestamp ? peli.estreno.toDate() : peli.estreno;
-			peli.actor = peli.actor instanceof DocumentReference ? await this.dbService.traerPorRef<Actor>(peli.actor) : peli.actor;
+			peli.actor = peli.actor instanceof DocumentReference ? await this.dbService.traerObjPorRef<Actor>(peli.actor) : peli.actor;
 		});
 		// arrAux.sort((p1, p2) => p1.estreno > p2.estreno ? 1 : -1);
 		this.peliculas = arrAux;
-	}
 
+		Swal.close();
+	}
+	
 	selecPelicula(pelicula: Pelicula) {
 		this.peliculaEvent.emit(pelicula);
 	}
